6ae8e0be995d0e3f849fbd1888b7e16a4c1f94c2
Richard <Richardtshi23@Gmail.com>
2026-03-16 21:49:44 +0200

    env change

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://holy-essence-backend-2.onrender.com/api', // <-- PASTE YOUR LIVE RENDER API URL HERE
+  apiUrl: 'https://frugool-backend.onrender.com/api', // <-- PASTE YOUR LIVE RENDER API URL HERE
   apiImageUrl: 'https://holy-essence-backend-2.onrender.com'
 };
